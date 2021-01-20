a5e6d4192422bf7bc6129a17f4446bf8d65467dc
Olivier Friard <olivier.friard@unito.it>
2021-01-20 17:31:28 +0100

    some work

---
+++ static/hide_image.js
@@ -0,0 +1,19 @@
+
+function hide_image(id, laps){
+
+window.timerID =  setInterval(function() {
+var aOpaque = document.getElementById('imageID').style.opacity;
+aOpaque = aOpaque-.1;
+
+aOpaque = aOpaque.toFixed(1);
+
+document.getElementById(id).style.opacity = aOpaque;
+
+if(document.getElementById(id).style.opacity<=0) clearInterval(window.timerID);
+}, laps);
+}
+
+window.onload = function(){hide_image("imageID", 10000);}
+
+
+
